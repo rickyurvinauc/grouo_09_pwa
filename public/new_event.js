dabf7fe3d538e94a25ebdb980a62a06518ff0e13
Ricardo Urvina <rurvinac@estudiante.uc.cl>
2024-05-02 14:26:46 -0400

    feat: create multiple input for friends name

---
+++ public/new_event.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Selecciona el botón "Add participant" por su ID
+    const addButton = document.getElementById('add-button');
+
+    // Selecciona el contenedor donde quieres agregar los nuevos divs por su ID
+    const containerFriends = document.getElementById('container-friends');
+
+    // Agrega un controlador de eventos al botón
+    addButton.addEventListener('click', function () {
+        // Crea un nuevo div con el mismo contenido
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML = `
+            <div class="grid md:grid-cols-1 mb-4">
+                <div class="flex">
+                    <div class="relative w-full">
+                        <input type="text"
+                            class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg rounded-s-gray-100 rounded-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+                            placeholder="Your friend's name" required/>
+                        <a 
+                            class="absolute top-0 end-0 p-2.5 h-full text-sm font-medium text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 delete-button">
+                            <i class="fas fa-trash-alt"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        // Agrega el nuevo div al contenedor
+        containerFriends.appendChild(newDiv);
+
+        // Agrega un controlador de eventos al botón de borrar
+        newDiv.querySelector('.delete-button').addEventListener('click', function () {
+            // Elimina el div de entrada
+            containerFriends.removeChild(newDiv);
+        });
+    });
+});
