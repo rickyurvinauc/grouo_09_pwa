15227201d79c24f6735000c2fc5f717e099b5b23
Ricardo Urvina <rurvinac@estudiante.uc.cl>
2024-05-03 22:16:54 -0400

    fix: fixes of sonarlint

--- public/js/event.js
+++ public/js/event.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
     const divs = document.getElementsByClassName('expense-user');
-   console.log({divs})
     for (let i = 0; i < divs.length; i++) {
         divs[i].addEventListener('click', function () {
             window.location.href = '/edit-transaction';
