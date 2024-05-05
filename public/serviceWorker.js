16654cad6dd5f2c79e9caff947686040a5e2a089
Ricardo Urvina <rurvinac@estudiante.uc.cl>
2024-05-04 22:53:29 -0400

    fix: serviceworker put on cache

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -7,11 +7,10 @@ const staticAssets = [
     './public/js/new_event.js',
     './public/images/favicon.png'
 ]
-
 self.addEventListener('install', async event => {
-    const cache = await caches.open('static-meme')
-    cache.addAll(staticAssets)
-})
+    const cache = await caches.open('static-meme');
+    cache.addAll(staticAssets);
+});
 
 self.addEventListener('fetch', event => {
     const {request} = event;
@@ -21,15 +20,17 @@ self.addEventListener('fetch', event => {
     } else {
         event.respondWith(networkFirst(request));
     }
+
 });
 
-async function cacheData(req) {
+async function cacheData(request) {
     const cachedResponse = await caches.match(request);
     return cachedResponse || fetch(request);
 }
 
-async function networkFirst(req) {
+async function networkFirst(request) {
     const cache = await caches.open('dynamic-meme');
+
     try {
         const response = await fetch(request);
         cache.put(request, response.clone());
@@ -37,4 +38,5 @@ async function networkFirst(req) {
     } catch (error) {
         return await cache.match(request);
     }
+
 }
